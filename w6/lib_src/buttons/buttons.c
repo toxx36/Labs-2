02ccf08b128b7101cf5be8d92f22bce5e4cb5701
Stanilav S <toxx136@gmail.com>
2019-05-15 02:23:25 +0300

    reduced nesting

--- w6/lib_src/buttons/buttons.c
+++ w6/lib_src/buttons/buttons.c
@@ -1,6 +1,8 @@
 #include "buttons.h"
 #include "string.h"
 
+/*! \brief  States of state machine
+ */
 typedef enum machine_state_e
 {
     M_WAITING,
@@ -8,9 +10,11 @@ typedef enum machine_state_e
     M_CLICKS_COUNTING,
     M_HOLDING,
     M_WAIT_RELEASE
-} machine_state_t; /// States of state machine
+} machine_state_t; 
 
-typedef struct button_state_s /// Contains all about button
+/*! \brief  Contains all about button
+ */
+typedef struct button_state_s 
 {
     machine_state_t state;
     uint8_t on_press;
@@ -24,13 +28,17 @@ typedef struct button_state_s /// Contains all about button
     uint32_t timer;
 } button_state_t;
 
-// Contains declared buttons
 static button_state_t btns[BTN_MAX_COUNT];
 buttons_phys_t *btns_phys;
 static uint8_t btn_count;
-static uint8_t btn_pressed;
 
 uint8_t button_fsm(uint8_t cur);
+
+static uint8_t m_wait_check(button_state_t *button, uint8_t phys_state);
+static uint8_t m_debounce_check(button_state_t *button, uint8_t phys_state);
+static uint8_t m_clicks_check(button_state_t *button, uint8_t phys_state);
+static uint8_t m_hold_check(button_state_t *button, uint8_t phys_state);
+static uint8_t m_release_check(button_state_t *button, uint8_t phys_state);
 
 int8_t buttons_periph_init(uint8_t rcc_count, uint32_t *rcc)
 {
@@ -44,20 +52,18 @@ int8_t buttons_periph_init(uint8_t rcc_count, uint32_t *rcc)
     {
         return -1;
     }
-    else
+    
+    /* Enable RSS's */
+    for (cur = 0; cur < rcc_count; cur++)
     {
-        // Enable RSS's
-        for (cur = 0; cur < rcc_count; cur++)
-        {
-            RCC_AHB1PeriphClockCmd((uint32_t)rcc[cur], ENABLE);
-        }
-        // Init buttons
-        for (cur = 0; cur < btn_count; cur++)
-        {
-            GPIO_InitStructure.GPIO_PuPd = btns_phys->pupd[cur];
-            GPIO_InitStructure.GPIO_Pin = btns_phys->pins[cur];
-            GPIO_Init(btns_phys->ports[cur], &GPIO_InitStructure);
-        }
+        RCC_AHB1PeriphClockCmd((uint32_t)rcc[cur], ENABLE);
+    }
+    /* Init physical buttons */
+    for (cur = 0; cur < btn_count; cur++)
+    {
+        GPIO_InitStructure.GPIO_PuPd = btns_phys->pupd[cur];
+        GPIO_InitStructure.GPIO_Pin = btns_phys->pins[cur];
+        GPIO_Init(btns_phys->ports[cur], &GPIO_InitStructure);
     }
     return 0;
 }
@@ -72,135 +78,40 @@ int8_t buttons_init(uint8_t count, buttons_phys_t *physical)
     {
         btn_count = (count > BTN_MAX_COUNT) ? BTN_MAX_COUNT : count;
         memset(&btns, 0, sizeof(btns));
-        btn_pressed = 0;
         btns_phys = physical;
     }
     return 0;
 }
 
-/// Button state machine which process each button
+/*! \brief  Button state machine which process each button
+ */
 uint8_t button_fsm(uint8_t cur)
 {
     uint8_t btn_update = 0;
-    uint8_t btn_state = GPIO_ReadInputDataBit(
-        btns_phys->ports[cur], btns_phys->pins[cur]); // read current btn state
+    /* read current btn state */
+    uint8_t phys_state = GPIO_ReadInputDataBit(
+        btns_phys->ports[cur], btns_phys->pins[cur]);
+    /* and inverse its state if needed */
     if (btns_phys->pupd[cur] == GPIO_PuPd_UP)
-        btn_state ^= 1;
+    {
+        phys_state ^= 1;
+    }
     switch (btns[cur].state)
     {
     case M_WAITING:
-        if (btn_state)
-        {
-            btns[cur].timer = 0;
-            btns[cur].pressed = 0;
-            btns[cur].state = M_DEBOUNCING;
-        }
-        if (btns[cur].click_timer != 0)
-        {
-            if (btns[cur].click_timer >= CLICK_TIME)
-            {
-                btns[cur].click_timer = 0;
-                btn_update = 1;
-            }
-            else
-            {
-                btns[cur].click_timer++;
-            }
-        }
+        btn_update = m_wait_check(btns + cur, phys_state);
         break;
     case M_DEBOUNCING:
-        btns[cur].timer++;
-        if (btns[cur].timer >= BOUNCE_TIME)
-        {
-            if (btn_state)
-            {
-                if (btns[cur].click_timer == 0)
-                {
-                    btns[cur].clicks = 0;
-                }
-                btns[cur].on_press = 1;
-                btns[cur].pressed = 1;
-                btns[cur].released = 0;
-                btns[cur].holded = 0;
-                btns[cur].hold_repeat = 1;
-                btns[cur].click_timer = BOUNCE_TIME;
-                btns[cur].state = M_CLICKS_COUNTING;
-                btn_update = 1;
-                btn_pressed++;
-            }
-            else
-            {
-                btns[cur].state = M_WAITING;
-            }
-        }
+        btn_update = m_debounce_check(btns + cur, phys_state);
         break;
     case M_CLICKS_COUNTING:
-        btns[cur].click_timer++;
-        if (btn_state)
-        {
-            btns[cur].timer++;
-            if (btns[cur].click_timer >= CLICK_TIME)
-            {
-                btns[cur].clicks += 1;
-                btns[cur].click_timer = 0;
-                btns[cur].state = M_HOLDING;
-                btn_update = 1;
-            }
-        }
-        else
-        {
-            btns[cur].on_press = 0;
-            btns[cur].released = 1;
-            btns[cur].clicks++;
-            btns[cur].state = M_WAITING;
-            btn_update = 1;
-            btn_pressed--;
-        }
+        btn_update = m_clicks_check(btns + cur, phys_state);
         break;
     case M_HOLDING:
-        if (btn_state)
-        {
-            btns[cur].timer++;
-            if (btns[cur].timer >= HOLD_TIME)
-            {
-                btns[cur].on_hold = 1;
-                btns[cur].holded = 1;
-                btns[cur].hold_repeat = 1;
-                btns[cur].timer = 0;
-                btns[cur].state = M_WAIT_RELEASE;
-                btn_update = 1;
-            }
-        }
-        else
-        {
-            btns[cur].on_press = 0;
-            btns[cur].released = 1;
-            btns[cur].state = M_WAITING;
-            btn_update = 1;
-            btn_pressed--;
-        }
-        break;
+        btn_update = m_hold_check(btns + cur, phys_state);
+       break;
     case M_WAIT_RELEASE:
-        if (btn_state)
-        {
-            btns[cur].timer++;
-            if (btns[cur].timer >= HOLD_REPEAT_TIME)
-            {
-                btns[cur].hold_repeat = 1;
-                btns[cur].timer = 0;
-                btn_update = 1;
-            }
-        }
-        else
-        {
-            btns[cur].on_press = 0;
-            btns[cur].on_hold = 0;
-            btns[cur].released = 1;
-            btns[cur].hold_repeat = 0;
-            btns[cur].state = M_WAITING;
-            btn_update = 1;
-            btn_pressed--;
-        }
+        btn_update = m_release_check(btns + cur, phys_state);
         break;
     default:
         break;
@@ -208,7 +119,135 @@ uint8_t button_fsm(uint8_t cur)
     return btn_update;
 }
 
-/// Func which process all buttons
+static uint8_t m_wait_check(button_state_t *button, uint8_t phys_state)
+{
+    if (phys_state)
+    {
+        button->timer = 0;
+        button->pressed = 0;
+        button->state = M_DEBOUNCING;
+    }
+    if (button->click_timer != 0)
+    {
+        if (button->click_timer >= CLICK_TIME)
+        {
+            button->click_timer = 0;
+            return 1;
+        }
+        else
+        {
+            button->click_timer++;
+        }
+    }
+    return 0;
+}
+
+static uint8_t m_debounce_check(button_state_t *button, uint8_t phys_state)
+{
+    button->timer++;
+    if (button->timer >= BOUNCE_TIME)
+    {
+        if (phys_state)
+        {
+            if (button->click_timer == 0)
+            {
+                button->clicks = 0;
+            }
+            button->on_press = 1;
+            button->pressed = 1;
+            button->released = 0;
+            button->holded = 0;
+            button->hold_repeat = 1;
+            button->click_timer = BOUNCE_TIME;
+            button->state = M_CLICKS_COUNTING;
+            return 1;
+        }
+        else
+        {
+            button->state = M_WAITING;
+        }
+    }
+    return 0;
+}
+
+static uint8_t m_clicks_check(button_state_t *button, uint8_t phys_state)
+{
+    button->click_timer++;
+    if (phys_state)
+    {
+        button->timer++;
+        if (button->click_timer >= CLICK_TIME)
+        {
+            button->clicks += 1;
+            button->click_timer = 0;
+            button->state = M_HOLDING;
+            return 1;
+        }
+    }
+    else
+    {
+        button->on_press = 0;
+        button->released = 1;
+        button->clicks++;
+        button->state = M_WAITING;
+        return 1;
+    }
+    return 0;
+}
+
+static uint8_t m_hold_check(button_state_t *button, uint8_t phys_state)
+{
+    if (phys_state)
+    {
+        button->timer++;
+        if (button->timer >= HOLD_TIME)
+        {
+            button->on_hold = 1;
+            button->holded = 1;
+            button->hold_repeat = 1;
+            button->timer = 0;
+            button->state = M_WAIT_RELEASE;
+            return 1;
+        }
+    }
+    else
+    {
+        button->on_press = 0;
+        button->released = 1;
+        button->state = M_WAITING;
+        return 1;
+    }
+    return 0;
+}
+
+static uint8_t m_release_check(button_state_t *button, uint8_t phys_state)
+{
+    if (phys_state)
+    {
+        button->timer++;
+        if (button->timer >= HOLD_REPEAT_TIME)
+        {
+            button->hold_repeat = 1;
+            button->timer = 0;
+            return 1;
+        }
+    }
+    else
+    {
+        button->on_press = 0;
+        button->on_hold = 0;
+        button->released = 1;
+        button->hold_repeat = 0;
+        button->state = M_WAITING;
+        return 1;
+    }
+    return 0;
+}
+
+/* Public functions */
+
+/*! \brief  Func which process all buttons
+ */
 uint8_t buttons_irq(void)
 {
     uint8_t cur;
@@ -223,7 +262,8 @@ uint8_t buttons_irq(void)
     return flag;
 }
 
-/// Is the button pressed now
+/*! \brief  Is the button pressed now
+ */
 uint8_t button_on_press(uint8_t cur)
 {
     if (btns[cur].on_press == 1)
@@ -233,7 +273,8 @@ uint8_t button_on_press(uint8_t cur)
     return 0;
 }
 
-/// Was the button pressed last time
+/*! \brief  Was the button pressed last time
+ */
 uint8_t button_pressed(uint8_t cur)
 {
     if (btns[cur].pressed == 1)
@@ -244,7 +285,8 @@ uint8_t button_pressed(uint8_t cur)
     return 0;
 }
 
-/// Is the button on hold
+/*! \brief  Is the button on hold
+ */
 uint8_t button_on_hold(uint8_t cur)
 {
     if (btns[cur].on_hold == 1)
@@ -254,7 +296,8 @@ uint8_t button_on_hold(uint8_t cur)
     return 0;
 }
 
-/// Was the button released
+/*! \brief  Was the button released
+ */
 uint8_t button_released(uint8_t cur)
 {
     if (btns[cur].released == 1)
@@ -265,7 +308,8 @@ uint8_t button_released(uint8_t cur)
     return 0;
 }
 
-/// Repeats button pressing over time, like PC keyboard
+/*! \brief  Repeats button pressing over time, like PC keyboard
+ */
 uint8_t button_with_repeat(uint8_t cur)
 {
     if (btns[cur].hold_repeat == 1)
@@ -276,7 +320,8 @@ uint8_t button_with_repeat(uint8_t cur)
     return 0;
 }
 
-/// Returns true if count value matches the clicks
+/*! \brief  Returns true if count value matches the clicks
+ */
 uint8_t button_clicks(uint8_t cur, uint8_t count)
 {
     if (btns[cur].clicks == count)
@@ -288,8 +333,9 @@ uint8_t button_clicks(uint8_t cur, uint8_t count)
     return 0;
 }
 
-/// Was button holded
-///\params once don't clear this state after reading
+/*! \brief  Was button holded
+ *  \params once don't clear this state after reading
+ */
 uint8_t button_holded(uint8_t cur, uint8_t once)
 {
     if (btns[cur].holded)
@@ -303,8 +349,9 @@ uint8_t button_holded(uint8_t cur, uint8_t once)
     return 0;
 }
 
-/// Returns count of clicks
-///\params consider_hold for using with Button_withRepeat function
+/*! \brief  Returns count of clicks
+ *  \params consider_hold for using with Button_withRepeat function
+ */
 uint8_t button_get_clicks(uint8_t cur, uint8_t consider_hold)
 {
     uint8_t tmp;
@@ -337,7 +384,14 @@ uint8_t button_get_clicks(uint8_t cur, uint8_t consider_hold)
     return 0;
 }
 
+/*! \brief  Returns current count of on press buttons
+ */
 uint8_t button_on_press_count(void)
 {
-    return btn_pressed;
+    uint8_t cur, count;
+    for(cur = 0; cur < btn_count; cur++)
+    {
+        count += btns[cur].on_press;
+    } 
+    return count;
 }
